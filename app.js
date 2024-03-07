df1da810d580b2326c1caf9b987867f0ca9bfd5a
AnasNdeem <126314120+AnasNdeem@users.noreply.github.com>
2024-03-06 22:04:40 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for (let i=0; i<6; i++){
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let intervalId;
+ const startChangingColor = function(){
+    if(!intervalId){
+        intervalId =  setInterval(changeColor, 1000);
+    }
+    function changeColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+ 
+ document.querySelector('#start').addEventListener('click', startChangingColor);
+ 
+ 
+
+
